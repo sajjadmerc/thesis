f85e9fff3c81fa76f0bcb3f59b9d547ee13c805b
Sephali Merchant <sephalimerchant@Sephalis-MacBook-Pro.local>
2019-07-30 00:33:19 -0400

    Add mobile hamburger click handler

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,16 @@ var navMenuIcon = document.querySelector(".nav-hamburger");
 navMenuIcon.classList.add("hidden");
 
 // determine to show full menu or icon
+var hidden = true;
+function menuHandler() {
+	if(!hidden) {
+		navMenu.classList.add('hidden');
+	} else {
+		navMenu.classList.remove('hidden');
+		navMenu.classList.add('mobile');
+	}
+	hidden = !hidden;
+}
 function reportWindowSize() {
   var width = window.innerWidth;
 
@@ -13,9 +23,11 @@ function reportWindowSize() {
 	navMenu.classList.add('hidden');
   } else {
   	navMenuIcon.classList.add('hidden');
+	navMenu.classList.remove('mobile');
 	navMenu.classList.remove('hidden');
   }
 }
 
+navMenuIcon.onclick = menuHandler;
 window.onresize = reportWindowSize;
 reportWindowSize();
